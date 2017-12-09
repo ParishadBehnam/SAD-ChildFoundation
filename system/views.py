e4a8be1fea7983adb4bd38617aec31f69965454e
afra amini <afra.amini@gmail.com>
2017-12-09 17:56:11 +0330

    Add Bootstrap

--- system/views.py
+++ system/views.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.http import Http404
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'show_madadjoo.html')
+
 # Create your views here.
