776b86f856a809dce385c38095e542fbec3966ba
Hannaneh <hannaneh.akrami95@gmail.com>
2017-12-15 14:29:37 +0330

    hamyar panel is added

--- active_user/urls.py
+++ active_user/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     url(r'show_a_madadjoo$', views.show_a_madadjoo, name='show_a_madadjoo'),
     url(r'show_a_hamyar$', views.show_a_hamyar, name='show_a_hamyar'),
     url(r'send_letter$', views.send_letter, name='send_letter'),
+    url(r'send_letter_hamyar$', views.send_letter_hamyar, name='send_letter_hamyar'),
     url(r'inbox$', views.inbox, name='inbox'),
+    url(r'inbox_hamyar$', views.inbox_hamyar, name='inbox_hamyar'),
     url(r'madadkar_panel$', views.madadkar_panel, name='madadkar_panel'),
+    url(r'hamyar_panel$', views.hamyar_panel, name='hamyar_panel'),
 ]
--- active_user/views.py
+++ active_user/views.py
@@ -8,22 +8,31 @@ def show_madadjoo(request):
     return render(request, 'show_madadjoo.html')
 
 def edit_madadjoo(request):
-    return render(request, 'edit_madadjoo.html')
+    return render(request, 'madadkar/edit_madadjoo.html')
 
 def add_madadjoo(request):
-    return render(request, 'add_madadjoo.html')
+    return render(request, 'madadkar/add_madadjoo.html')
 
 def show_a_madadjoo(request):
-    return render(request, 'show_a_madadjoo.html')
+    return render(request, 'madadkar/show_a_madadjoo.html')
 
 def show_a_hamyar(request):
     return render(request, 'show_a_hamyar.html')
 
 def send_letter(request):
-    return render(request, 'send_letter.html')
+    return render(request, 'madadkar/send_letter.html')
+
+def send_letter_hamyar(request):
+    return render(request, 'hamyar/send_letter.html')
 
 def inbox(request):
-    return render(request, 'inbox.html')
+    return render(request, 'madadkar/inbox.html')
+
+def inbox_hamyar(request):
+    return render(request, 'hamyar/inbox.html')
 
 def madadkar_panel(request):
-    return render(request, 'madadkar_panel.html')
+    return render(request, 'madadkar/madadkar_panel.html')
+
+def hamyar_panel(request):
+    return render(request, 'hamyar/hamyar_panel.html')
