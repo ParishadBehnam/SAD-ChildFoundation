0e0c1c66cca0445586460494bade2c2640b45a81
afra amini <afra.amini@gmail.com>
2017-12-21 21:43:33 +0330

    Database set

--- ChildF/settings.py
+++ ChildF/settings.py
@@ -142,3 +142,11 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
