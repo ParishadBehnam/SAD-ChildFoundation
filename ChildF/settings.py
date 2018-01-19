a36b1172af7df5fdcf91c8fc4b9569622faa80a3
afra amini <afra.amini@gmail.com>
2018-01-19 21:12:15 +0330

    Change settings

--- ChildF/settings.py
+++ ChildF/settings.py
@@ -128,5 +128,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 # Configure Django App for Heroku.
-import django_heroku
-django_heroku.settings(locals())
+# import django_heroku
+# django_heroku.settings(locals())
