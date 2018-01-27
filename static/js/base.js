954b440fc64a54dc6ff0bc3b2cd3e3dcf1853afc
afra amini <afra.amini@gmail.com>
2018-01-27 03:56:42 +0330

    Some unfinished works on madadkar_madadjoo

--- static/js/base.js
+++ static/js/base.js
@@ -2,44 +2,48 @@
  * Created by afra on 12/13/17.
  */
 
-$('.emergency-need-admin').click(function() {
+$('.emergency-need-admin').click(function () {
     if ($(this).hasClass("badge-secondary")) {
         $(this).removeClass('badge-secondary').addClass('badge-danger');
         //$('<p>نیاز فوری برای تایید به مدیر ارسال می‌شود</p>').appendTo($(this).parent())
-    }else {
+    } else {
         $(this).removeClass('badge-danger').addClass('badge-secondary');
-        $(this).parent().contents().filter(function(){ return this.tagName == 'P'; }).remove();
+        $(this).parent().contents().filter(function () {
+            return this.tagName == 'P';
+        }).remove();
 
     }
 });
 
-$('.emergency-need').click(function() {
+$('.emergency-need').click(function () {
     if ($(this).hasClass("badge-secondary")) {
         $(this).removeClass('badge-secondary').addClass('badge-danger');
         $('<p>نیاز فوری برای تایید به مدیر ارسال می‌شود</p>').appendTo($(this).parent())
-    }else {
+    } else {
         $(this).removeClass('badge-danger').addClass('badge-secondary');
-        $(this).parent().contents().filter(function(){ return this.tagName == 'P'; }).remove();
+        $(this).parent().contents().filter(function () {
+            return this.tagName == 'P';
+        }).remove();
 
     }
 });
 
-$(function(){
-    $(document).on("change","input", function(){
-        var allGood=true;
-        var lastInputField=0;
-        $("input").each(function() {
-            if ($(this).val() =="") {
-                allGood=false;
+$(function () {
+    $(document).on("change", "input", function () {
+        console.log("hiiii");
+        var allGood = true;
+        var lastInputField = 0;
+        $("input").each(function () {
+            if ($(this).val() == "") {
+                allGood = false;
                 return false;
             }
             lastInputField++;
         });
 
         if (allGood) {
-            $("<span>" + lastInputField + "<input type='text' id='lastinputfieldId" + lastInputField +"'" +
-              "name='lastinputfieldName" + lastInputField + "'></span>").appendTo("form");
+            $("<span>" + lastInputField + "<input type='text' id='lastinputfieldId" + lastInputField + "'" +
+                "name='lastinputfieldName" + lastInputField + "'></span>").appendTo("form");
         }
     });
 });
-​
